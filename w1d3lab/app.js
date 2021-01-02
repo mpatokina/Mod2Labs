a5edbb718e565d1f414e380b152781ff08fd37bc
Maria Patokina <mpatokina@hotmail.com>
2021-01-02 15:16:48 -0500

    my second commit

--- w1d3lab/app.js
+++ w1d3lab/app.js
@@ -16,43 +16,25 @@ class Tardis extends React.Component {
   }
   render() {
     return (
-      <div>
+         <div onClick={ this.changeFormat } className="tardis-component">
+            <h1>{ this.state.string }</h1>
+          </div>    
+         )
+        }
+}
+class Final extends React.Component {
+  render() {
+    return (
+      <div> 
         <div>
-          <div onClick={ this.changeFormat } className="tardis-component">
-            <div>
-              <h1>{ this.state.string }</h1>
-            </div>
-            <Tardis1 />
+          <div>
+                <Tardis />              
+                <Tardis />
           </div>
         </div>
       </div>
     )
   }
-}
-
-class Tardis1 extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isCased1: false,
-            string1: 'Time and Relative Dimension in Space'
-        }
-        this.changeFormat = this.changeFormat.bind(this);
-    }
-    changeFormat() {
-        if(!this.state.isCased1) {
-          this.setState({ string1: this.state.string1.toUpperCase(), isCased1: !this.state.isCased1 })
-        } else {
-          this.setState({ string1: this.state.string1.toLowerCase(), isCased1: !this.state.isCased1 })
-        }
-      }
-      render() {
-        return (
-          <div onClick={ this.changeFormat } className="tardis-component1">
-            <h1>{ this.state.string1 }</h1>
-          </div>
-        )
-      }
-    }
+}    
     
-ReactDOM.render(<Tardis />, document.getElementById('container'));
+ReactDOM.render(<Final />, document.getElementById('container'));
